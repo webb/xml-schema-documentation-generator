b8a90997a3ea01440fcd94359f5173dc57b7863c
Webb Roberts <webb.roberts@gtri.gatech.edu>
2018-06-13 12:00:05 -0400

    Merged build process into a working makefile.

--- generate.mk
+++ generate.mk
@@ -1,20 +1,27 @@
 
-install_dir=../repo
-build_dir=build
+# input variables: ###############################################################
+# install_dir: where content gets published
+# build_dir: temporary directory for creating content
+# definitions_mk: dynamically generated makefile definitions listing namespaces
+#     and components
 
-include build/definitions.mk
+include ${definitions_mk}
 
-all_files = ${build_dir}/index.html ${namespaces:%=${build_dir}/%/index.html} ${components:%=${build_dir}/%/index.html}
+all_files = \
+  ${build_dir}/index.html \
+  ${namespaces:%=${build_dir}/%/index.html} \
+  ${components:%=${build_dir}/%/index.html}
+
 install_files = ${all_files:${build_dir}/%=${install_dir}/%}
 
-all: ${all_files}
-
-install: ${install_files}
-
-diagrams: ${components:%=${build_dir}/%/diagram.png.base64} ${components:%=${build_dir}/%/diagram.map}
-
+#############################################################################
+.PHONY: dirs
 dirs:
 	mkdir -p ${components}
+
+#############################################################################
+.PHONY: diagrams
+diagrams: ${components:%=${build_dir}/%/diagram.png.base64} ${components:%=${build_dir}/%/diagram.map}
 
 ${build_dir}/%/diagram.png.base64: ${build_dir}/%/diagram.png
 	base64 --wrap=0 $< > $@
@@ -22,9 +29,14 @@ ${build_dir}/%/diagram.png.base64: ${build_dir}/%/diagram.png
 ${build_dir}/%/diagram.png ${build_dir}/%/diagram.map: ${build_dir}/%/diagram.dot
 	dot -Tpng -o${build_dir}/$*/diagram.png -Tcmapx -o${build_dir}/$*/diagram.map $<
 
+#############################################################################
+.PHONY: install
+install: ${install_files}
+
 ${install_dir}/%: ${build_dir}/%
 	mkdir -p ${dir $@}
 	cp $< $@
+
 
 
 
